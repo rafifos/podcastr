5374a0efc750bd8ea09b59c1611959439bef42bd
Rafael Julio <development@rafifos.dev>
2021-04-20 23:57:20 -0300

    feat: implement the homepage
    Signed-off-by: Rafael Julio <development@rafifos.dev>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,24 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import "modern-normalize";
+import Head from "next/head";
+import Header from "components/Header";
+import Player from "components/Player";
+
+import styles from "styles/pages/app.module.scss";
+
+function App({ Component, pageProps }) {
+  return (
+    <div className={styles.App}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+
+      <Player />
+    </div>
+  );
 }
 
-export default MyApp;
+export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
-import Head from "next/head";
+import type { GetStaticProps, GetStaticPropsContext } from "next";
 
-export default function Home() {
-  return (<h1>Hello world!</h1>);
+function Home() {
+  return <h1>A</h1>;
 }
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 80 * 8,
+  };
+};
+
+export default Home;
